6882ff50bca55ccc095a1951623edebfa107ba4a
CarlosRoa <roapalaciocarlos@gmail.com>
2026-08-07 16:34:27 -0400

    Fix typo in transform method call

--- healthcare_outcome_mlops/utils/ml_utils/model/estimator.py
+++ healthcare_outcome_mlops/utils/ml_utils/model/estimator.py
@@ -13,7 +13,7 @@ class HealthcareModel:
         
     def predict(self, x):
         try: 
-            x_transform = self.preprocessor.traansform(x)
+            x_transform = self.preprocessor.transform(x)
             y_hat = self.model.predict(x_transform)
             return y_hat
         except Exception as e:
